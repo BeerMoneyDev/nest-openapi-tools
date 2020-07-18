07fe45e5c49f407d7e07d226b685baf78debe952
Kerry Ritter <kritter@lumeris.com>
2020-07-18 08:04:41 -0500

    master
    rename

--- src/aws-serverless-tools.module.ts
+++ src/aws-serverless-tools.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ApiGatewayOpenApi } from './api-gateway-openapi.service';
-import { OpenapiFileGeneratorService } from './openapi-file-generator/openapi-file-generator.service';
-import { OpenapiAngularClientGeneratorService } from './openapi-angular-client-generator/openapi-angular-client-generator.service';
+import { OpenApiFileGeneratorService } from './openapi-file-generator/openapi-file-generator.service';
+import { OpenApiAngularClientGeneratorService } from './openapi-angular-client-generator/openapi-angular-client-generator.service';
 
 @Module({
   providers: [
     ApiGatewayOpenApi,
-    OpenapiFileGeneratorService,
-    OpenapiAngularClientGeneratorService,
+    OpenApiFileGeneratorService,
+    OpenApiAngularClientGeneratorService,
   ],
   exports: [
     ApiGatewayOpenApi,
